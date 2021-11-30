eb2f36089f4ef9f2bec06647b459ccf1f2e5ecc1
Aaron Williams <aaronrw4@yahoo.com>
2021-11-29 23:28:36 -0800

    first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const stats = {
+    'Heinicke': {'avg': 18.3, 'cost': 14700}, 
+    'McLaurin': {'avg': 17.2, 'cost': 15900}, 
+    'Wilson': {'avg': 17, 'cost': 16500}, 
+    'Metcalf': {'avg': 16.1, 'cost': 13200}, 
+    'Lockett': {'avg': 14.9, 'cost': 15300}, 
+    'Gibson': {'avg': 12.8, 'cost': 12900}, 
+    'McKissic': {'avg': 10.5, 'cost': 10800}, 
+    'Slye': {'avg': 9, 'cost': 6300}, 
+    'Thomas': {'avg': 8.7, 'cost': 7200}, 
+    'Collins': {'avg': 7.5, 'cost': 9900}, 
+    'Everett': {'avg': 6.9, 'cost': 9300}, 
+    'Carter': {'avg': 6.4, 'cost': 8400}, 
+    'Myers': {'avg': 5.6, 'cost': 6000}, 
+    'Swain': {'avg': 4.8, 'cost': 3300}, 
+    'Dissly': {'avg': 3.5, 'cost': 900}
+}
